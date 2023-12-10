5f9fda4feee9fef0e798e595b3dae4bc1aa0de49
tatendabakozw <tatendabakozw@gmail.com>
2023-12-10 17:44:18 +0200

    fix: send message about page

--- src/utils/apiUrl.ts
+++ src/utils/apiUrl.ts
@@ -1,2 +1,2 @@
 // export const aapiUrl = `http://localhost:7000`
-export const apiUrl = `https://enroll-mate-backend.onrender.com/reply`
+export const apiUrl = `https://enroll-mate-backend.onrender.com`
