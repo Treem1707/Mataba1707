c4ec72d678b8359fb7466f7de2248ad146920029
tatendabakozw <tatendabakozw@gmail.com>
2023-12-14 00:18:49 +0200

    feaure: added documentts input

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -219,11 +219,31 @@ const Register = (props: Props) => {
                 <p className="text-sm font-medium text-pink-600 pb-2 ">
                   Upload documents
                 </p>
-                <input
-                  type="text"
-                  className="bg-purple-200 col-span-1 rounded-full px-2 py-3 w-full"
-                  placeholder=""
-                />
+                <div className="flex items-center justify-center w-full">
+                  <label
+                    htmlFor="dropzone-file"
+                    className="flex flex-col items-center justify-center w-full bg-purple-200 col-span-1 rounded-full px-2 py-3 cursor-pointer"
+                  >
+                    <div className="flex flex-col items-center justify-center">
+                      <svg
+                        className="w-6 h-6 text-slate-600 "
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 20 16"
+                      >
+                        <path
+                          stroke="currentColor"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                        />
+                      </svg>
+                    </div>
+                    <input id="dropzone-file" type="file" className="hidden" />
+                  </label>
+                </div>
               </div>
 
               <div className="flex w-full col-span-2 flex-col items-end">
